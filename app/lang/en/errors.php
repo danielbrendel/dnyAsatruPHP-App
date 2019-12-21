52254f52edde0cb899716f86cc12146de2fb8272
Daniel Brendel <dbrendel1988@yahoo.com>
2019-12-21 15:52:18 +0100

    New language file for errors

---
+++ app/lang/en/errors.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+	Asatru PHP - English language file for errors
+*/
+
+return [
+    'csrf_token_invalid' => 'CSRF token is missing or invalid',
+    'item_required' => 'Item %s is required.',
+    'item_email' => 'Item %s must be a valid E-Mail address',
+    'item_too_short' => 'Item length of %s must be greater than %u',
+    'item_too_large' => 'Item length of %s must be less than %u',
+    'item_datetime' => 'Item %s is not a valid datetime object',
+    'item_number' => 'Item %s is not a valid number'
+];

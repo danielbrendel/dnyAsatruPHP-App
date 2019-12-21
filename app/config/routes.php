54b404bf4ba8ea3296bf5fb9028e70b563dd4f16
Daniel Brendel <dbrendel1988@yahoo.com>
2019-12-21 01:41:01 +0100

    New routing layout & update doc

--- app/config/routes.php
+++ app/config/routes.php
@@ -6,17 +6,17 @@
     Add here all your needed routes.
 
     Schema:
-        <url> = [<method>, controller_file@controller_method]
+        [<url>, <method>, controller_file@controller_method]
     Example:
-        /my/route = [get, mycontroller@index]
-        /my/route/with/{param1}/and/{param2} = [get, mycontroller@another]
+        [/my/route, get, mycontroller@index]
+        [/my/route/with/{param1}/and/{param2}, get, mycontroller@another]
     Explanation:
         Will call index() in app\controller\mycontroller.php if request is 'get'
         Every route with $ prefix is a special route
 */
 
 return [
-    '/' => array('GET', 'index@index'),
-    '/index' => array('GET', 'index@index'),
-    '$404' => array('ANY', 'error404@index')
+    array('/', 'GET', 'index@index'),
+    array('/index', 'GET', 'index@index'),
+    array('$404', 'ANY', 'error404@index')
 ];

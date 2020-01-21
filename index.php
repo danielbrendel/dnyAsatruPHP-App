9bb68254dca638300bbc3b64747603f8cde4caa9
Daniel Brendel <dbrendel1988@yahoo.com>
2020-01-21 21:51:21 +0100

    Update doc

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 /*
     Asatru PHP (dnyAsatruPHP) developed by Daniel Brendel
     
-    (C) 2019 by Daniel Brendel
+    (C) 2019 - 2020 by Daniel Brendel
     
     Version: 0.1
     Contact: dbrendel1988<at>yahoo<dot>com
@@ -13,10 +13,10 @@
     License: see LICENSE.txt
 */
 
-//If composer is installed we utilize its autoloader
+//If composer is installed then utilize its autoloader
 if (file_exists(__DIR__ . '/vendor/autoload.php')) {
     require_once __DIR__ . '/vendor/autoload.php';
 }
 
-//Include the framework gateway in order to process the application
-require_once __DIR__ . '/vendor/danielbrendel/asatru-php-framework/src/gateway.php';
+//Include the framework bootstrap script in order to process the application
+require_once __DIR__ . '/vendor/danielbrendel/asatru-php-framework/src/bootstrap.php';

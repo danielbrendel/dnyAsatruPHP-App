8657e906663344cbdc030cbf9e52795141b862ef
Daniel Brendel <dbrendel1988@gmail.com>
2020-02-10 00:21:42 +0100

    GitHub Link

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     
     Version: 0.1
     Contact: dbrendel1988<at>gmail<dot>com
-    GitHub: https://github.com/danielbrendel
+    GitHub: https://github.com/danielbrendel/
     
     License: see LICENSE.txt
 */

cbdc95868b582f2f5c45b3404a17e26989835596
Daniel Brendel <dbrendel1988@gmail.com>
2020-02-18 20:14:17 +0100

    Update license infos

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     Contact: dbrendel1988<at>gmail<dot>com
     GitHub: https://github.com/danielbrendel/
     
-    License: see LICENSE.txt
+    Released under the MIT license
 */
 
 //If composer is installed then utilize its autoloader

f835d5b519746b43b7428f6abc04035e2681be07
Daniel Brendel <dbrendel1988@gmail.com>
2021-08-06 15:00:30 +0200

    Fix comments

--- app/controller/error404.php
+++ app/controller/error404.php
@@ -17,8 +17,8 @@ class Error404Controller extends BaseController {
 
 		//Generate the 404 view
 		$v = new Asatru\View\ViewHandler();
-		$v->setLayout('layout') //The layout file. Will be \app\view\layout.php
-			->setYield('yield', 'error/404'); //The index yield. Will be \app\view\index.php
+		$v->setLayout('layout') //The layout file. Will be \app\views\layout.php
+			->setYield('yield', 'error/404'); //The index yield. Will be \app\views\error\404.php
 		
 		return $v; //Pass the object to the engine
 	}

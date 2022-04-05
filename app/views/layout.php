294fff810d0095dec257d2078429cb74e046a463
Daniel Brendel <dbrendel1988@gmail.com>
2022-04-05 13:52:03 +0200

    Title

--- app/views/layout.php
+++ app/views/layout.php
@@ -7,7 +7,7 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-with, initial-scale=1.0">
 		
-		<title>{{ ASATRU_FW_NAME }}</title>
+		<title>{{ ASATRU_FW_NAME }} - The lightweight MVC web framework</title>
 
 		<script src="{{ asset('js/app.js') }}"></script>
 	</head>

42b1c3e80208b752279dd75bd95ce924c457308d
Daniel Brendel <dbrendel1988@gmail.com>
2024-03-12 15:25:12 +0100

    Include more components & improve content

--- app/lang/en/app.php
+++ app/lang/en/app.php
@@ -10,5 +10,5 @@
 
 return [
 	'welcome' => 'Welcome to the Asatru PHP framework',
-	'description' => 'You have successfully installed the Asatru PHP framework. You may now want to start developing your app. Consider the documentation located in /doc for help. Have fun with the framework!'
+	'description' => 'You have successfully installed the Asatru PHP framework. You may now want to start developing your app. Consider reading the documentation located in /doc for help. Have fun with the framework!'
 ];

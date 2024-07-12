cec930bad06b6c996d55c67710dc9b6b8b6be42e
Daniel Brendel <dbrendel1988@gmail.com>
2024-07-12 15:50:12 +0200

    Require modules & base test

---
+++ app/tests/IndexTest.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+    Testcase for Test Index
+*/
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * This class holds your test methods
+ */
+class IndexTest extends Asatru\Testing\Test
+{
+    /**
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /**
+     * @return void
+     */
+    public function testIndex()
+    {
+        $this->assertTrue(true);
+    }
+}
+    

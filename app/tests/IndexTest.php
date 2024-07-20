e5002655f9a565780074c9d4b5e15e2007e607a8
Daniel Brendel <dbrendel1988@gmail.com>
2024-07-20 15:45:05 +0200

    Fix tests

--- app/tests/IndexTest.php
+++ app/tests/IndexTest.php
@@ -24,8 +24,24 @@ class IndexTest extends Asatru\Testing\Test
      */
     public function testIndex()
     {
-        $response = $this->request(Asatru\Testing\Test::REQUEST_GET, '/auth')->getResponse();
+        $response = $this->request(Asatru\Testing\Test::REQUEST_GET, '/')->getResponse();
+
         $this->assertInstanceOf(Asatru\View\ViewHandler::class, $response);
+        $this->assertStringContainsString('<h1>' . __('app.welcome') . '</h1>', $response->out(true));
+    }
+
+    /**
+     * @return void
+     */
+    public function testError404()
+    {
+        $checkUrl = '/does/not/exist';
+
+        $response = $this->request(Asatru\Testing\Test::REQUEST_GET, $checkUrl)->getResponse();
+
+        $this->assertInstanceOf(Asatru\View\ViewHandler::class, $response);
+        $this->assertStringContainsString('<h1>Error 404</h1>', $response->out(true));
+        $this->assertStringContainsString('<p>The requested resource ' . env('APP_URL') . $checkUrl . ' was not found on the server.</p>', $response->out(true));
     }
 }
     

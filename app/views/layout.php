2aa0945230bfce37c66406f0885532adbe890ae5
Daniel Brendel <dbrendel1988@gmail.com>
2025-07-05 22:32:43 +0200

    Proper wording

--- app/views/layout.php
+++ app/views/layout.php
@@ -5,7 +5,7 @@
 <html lang="{{ getLocale() }}">
 	<head>
 		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-with, initial-scale=1.0">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		
 		<title>{{ ASATRU_FW_NAME }} - The lightweight MVC web framework</title>
 
